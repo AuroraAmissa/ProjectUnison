844825cb23c898992ef6feb2b8013e70423f29cb
Lymia Kanokawa <lymia@lymia.moe>
2024-04-20 10:03:39 -0700

    Remove highlighting support from build scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash --pure -p soupault -p pandoc -p minify -p nix -p highlight
+#! nix-shell -i bash --pure -p soupault -p pandoc -p minify -p nix
 
 set -eu
 
